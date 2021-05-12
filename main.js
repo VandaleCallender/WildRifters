87ffdf3499b8642fad7a81773194ab5180a90d2e
Vandale Callender <info@vandale.tech>
2021-05-11 18:23:41 -0700

    Countdown
    Countdown for 1st day of soft release

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("May 17, 2021 12:00:00").getTime();
+let launchDate = new Date("May 15, 2021 15:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
